933efcc8ddd47751c209444b88e896cf8df3f32c
Cassandra  Evans <cassie@Cassandras-MacBook-Pro.local>
2018-11-11 18:41:41 +0000

    Set up black divs and add 3rd transition

--- src/black-friday-300x600/js/banner.animation.js
+++ src/black-friday-300x600/js/banner.animation.js
@@ -15,8 +15,10 @@ Banner.prototype.start = function () {
     'images/logo1.svg',
     'images/txt1.svg',
     'images/txt2.svg',
+    'images/txt3.svg',
     'images/img1.jpg',
     'images/img2.jpg',
+    'images/img3.jpg',
   ];
 
   var _this = this;
@@ -33,6 +35,11 @@ Banner.prototype.start = function () {
  * Create dom elements.
  */
 Banner.prototype.createElements = function () {
+  this.img3 = this.smartObject({
+    backgroundImage: 'images/img3.jpg',
+    retina: true,
+    parent: this.banner
+  });
   this.img2 = this.smartObject({
     backgroundImage: 'images/img2.jpg',
     retina: true,
@@ -41,6 +48,12 @@ Banner.prototype.createElements = function () {
   this.img1 = this.smartObject({
     backgroundImage: 'images/img1.jpg',
     retina: true,
+    parent: this.banner
+  });
+  this.divTop = this.smartObject({
+    parent: this.banner
+  });
+  this.divBottom = this.smartObject({
     parent: this.banner
   });
   this.logoTop = this.smartObject({
@@ -55,6 +68,11 @@ Banner.prototype.createElements = function () {
   });
   this.txt2 = this.smartObject({
     backgroundImage: 'images/txt2.svg',
+    retina: true,
+    parent: this.banner
+  });
+  this.txt3 = this.smartObject({
+    backgroundImage: 'images/txt3.svg',
     retina: true,
     parent: this.banner
   });
@@ -75,9 +93,13 @@ Banner.prototype.setup = function () {
   this.logoTop.set({ width: '100%', height: '100%', });
   this.txt1.set({ width: '100%', height: '100%', });
   this.txt2.set({ width: '100%', height: '100%', });
+  this.txt3.set({ width: '100%', height: '100%', });
   this.img1.set({ width: '100%', height: '100%' });
   this.img2.set({ width: '100%', height: '100%', });
+  this.img3.set({ width: '100%', height: '100%', });
   this.div.set({ width: '100%', height: '100%', backgroundColor: "#000" });
+  this.divTop.set({ width: '100%', height: '15%', backgroundColor: "#000" });
+  this.divBottom.set({ width: '100%', height: '23%', bottom: "0", top: 'unset', backgroundColor: "#000" });
   this.logo.set({ width: '100%', height: '100%', });
 };
 
@@ -99,7 +121,7 @@ Banner.prototype.animate = function () {
   }
 
   var loops = 0;
-  var maxLoops = 2;
+  var maxLoops = 1;
 
   var banner = document.querySelector('.banner')
 
@@ -116,6 +138,10 @@ Banner.prototype.animate = function () {
     .add(TweenMax.to(this.img1, 1, { xPercent: 100, ease: Expo.easeInOut, delay: -0.1 }))
     .add(TweenMax.from(this.img2, 1, { xPercent: -100, ease: Expo.easeInOut, delay: -1 }))
     .add(TweenMax.from(this.txt2, 0.4, { autoAlpha: 0, ease: Expo.easeIn, delay: -0.2 }))
+    .add(TweenMax.to(this.txt2, 0.4, { autoAlpha: 0, ease: Expo.easeIn, delay: 2 }))
+    .add(TweenMax.to(this.img2, 1, { xPercent: 100, ease: Expo.easeInOut, delay: -0.1 }))
+    .add(TweenMax.from(this.img3, 1, { xPercent: -100, ease: Expo.easeInOut, delay: -1 }))
+    .add(TweenMax.from(this.txt3, 0.4, { autoAlpha: 0, ease: Expo.easeIn, delay: -0.2 }))
     .call(function () {
 
       if (loops === maxLoops) {
@@ -124,10 +150,8 @@ Banner.prototype.animate = function () {
       loops++;
 
     }.bind(_this))
-    .add(TweenMax.to(this.txt2, 0.4, { autoAlpha: 0, ease: Expo.easeIn, delay: 2 }))
+    .add(TweenMax.to(this.txt3, 0.4, { autoAlpha: 0, ease: Expo.easeIn, delay: 2 }))
     .add(TweenMax.to(this.div, 0.1, { autoAlpha: 1, }))
     .add(TweenMax.to(this.div, 0.8, { xPercent: 0, ease: Expo.easeIn, delay: -0.2 }))
     .add(TweenMax.to(this.logo, 0.2, { autoAlpha: 1, ease: Expo.easeIn, delay: 0.4 }))
-
-
 };
